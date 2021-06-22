2bb1c18b64479bc97e2c1fd2518a3649f1b95e72
Johnny Murillo <jossters@gmail.com>
2021-06-22 01:58:44 -0700

    wip

--- database/seeds/02-users.js
+++ database/seeds/02-users.js
@@ -1,13 +1,14 @@
+const bcrypt = require('bcryptjs')
 exports.seed = function(knex) {
   return knex('users')
     .then(function () {
       return knex('users').insert([
-        {username: 'Rick J. Walters', email: 'RickJWalters@jourrapide.com', password:'1234', role:'instructor'},
-        {username: 'Fred J. Enlow', email: 'FredJEnlow@dayrep.com', password:'4321', role:'student'},
-        {username: 'Robert W. Logan', email: 'RobertWLogan@armyspy.com', password: '12345', role:'student'},
-        {username: 'Matthew E. Brew', email: 'MatthewEBrew@armyspy.com', password: '54321', role:'student'},
-        {username: 'Betty J. Cummings', email: 'BettyJCummings@armyspy.com', password: '654321', role:'student'},
-        {username: 'Cecil K. McNab', email: 'CecilKMcNab@teleworm.us', password: '123456', role:'instructor'}
+        {username: 'Rick J. Walters', email: 'RickJWalters@jourrapide.com', password: bcrypt.hashSync('46487',10), role:'instructor'},
+        {username: 'Fred J. Enlow', email: 'FredJEnlow@dayrep.com', password: bcrypt.hashSync('494784',10), role:'student'},
+        {username: 'Robert W. Logan', email: 'RobertWLogan@armyspy.com', password: bcrypt.hashSync('48949',10), role:'student'},
+        {username: 'Matthew E. Brew', email: 'MatthewEBrew@armyspy.com', password: bcrypt.hashSync('43848',10), role:'student'},
+        {username: 'Betty J. Cummings', email: 'BettyJCummings@armyspy.com', password: bcrypt.hashSync('9048',10), role:'student'},
+        {username: 'Cecil K. McNab', email: 'CecilKMcNab@teleworm.us', password: bcrypt.hashSync('028207',10), role:'instructor'}
       ]);
     });
 };
